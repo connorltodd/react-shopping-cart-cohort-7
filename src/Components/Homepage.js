088570b09586d094c9e7b23347e47f19b63cfad3
Connor Todd <connortodd0304@gmail.com>
2022-07-21 18:19:27 +0100

    lifecycles react lesson completed

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,7 +4,32 @@ import Product from "./Product";
 
 function Homepage () {
     // Experiment with state in react
+    // React use state is a react hook
     const [products, setProducts] = React.useState([]);
+
+    // React useEffect hook
+
+    // componentDidMount example
+    React.useEffect(() => {
+        console.log('we are fetching our products')
+        fetchProducts()
+    }, []);
+
+    // runs every render
+    // React.useEffect(() => {});
+
+    // runs after update of products but can be used for any state value
+    React.useEffect(() => {
+        console.log('We have a full list of products')
+    }, [products]);
+
+    // componentWillUnmount
+    // React.useEffect(() => {
+        
+    //     return () => {
+
+    //     }
+    // })
   
 
     const fetchProducts = () => {
@@ -22,10 +47,10 @@ function Homepage () {
             {/* Within react you need to return a value or null */}
             {/* For loop / if statement are statements */}
             {/* ternary operators and array methods return a value */}
-            <button onClick={fetchProducts}>Show Products</button>
+            {/* <button onClick={fetchProducts}>Show Products</button> */}
             {
             // Put some products inside the state and map them with or without an api
-              products.map((productObject) => <Product  product={productObject} />)  
+              products.map((productObject) => <Product key={productObject.id}  product={productObject} />)  
             }
             
         </div>
